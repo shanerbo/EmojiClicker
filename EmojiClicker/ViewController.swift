4d32cdb95f1dae0c655ffb49ee6b90083a575099
erbos <erbos@sfu.ca>
2019-01-28 21:49:55 -0800

    initialize country dictionary

--- EmojiClicker/ViewController.swift
+++ EmojiClicker/ViewController.swift
@@ -16,6 +16,13 @@ class ViewController: UIViewController {
         
     }
     @IBAction func showMessage(sender: UIButton){
+        // Create dictionary of emoji icons
+        var emojiDic : [String : String] = ["🇨🇳" : "China", "🇯🇵" :"Japan", "🇰🇷" : "Korea",  "🇹🇼" : "Taiwan"]
+        var countryToLookUp = "🇹🇼"
+        var country = emojiDic[countryToLook]
+        
+        
+        
         let alertController = UIAlertController(title: "Welcome to my app", message: "Hello World", preferredStyle: UIAlertController.Style.alert)
         alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
         present(alertController, animated: true, completion: nil)
