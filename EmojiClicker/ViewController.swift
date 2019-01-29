3c1c677ab80370a8f05e63f387d9b81df3ee06b0
erbos <erbos@sfu.ca>
2019-01-28 20:39:50 -0800

    Once button is clicked, there will be a alert window pop Connect button with function by holding control and dragging button to top of storyboard

--- EmojiClicker/ViewController.swift
+++ EmojiClicker/ViewController.swift
@@ -13,8 +13,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
+    @IBAction func showMessage(sender: UIButton){
+        let alertController = UIAlertController(title: "Welcome to my app", message: "Hello World", preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+        
+    }
 
 }
 
