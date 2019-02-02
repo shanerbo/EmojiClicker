03b32141f0301a1a25f7a7691d1a87f2b4454745
erbos <erbos@sfu.ca>
2019-02-01 21:32:38 -0800

    Need to create a table cell and name its identifier as ChecklistItem which matches identifier in ChecklistViewController.swift. After assigning table cell as "ChecklistItem" the name of cell in storyboard will change form "table cell view" to "ChecklistItem". This is important, because is causing no check mark in simulator.

--- EmojiClicker/ChecklistViewController.swift
+++ EmojiClicker/ChecklistViewController.swift
@@ -1,9 +1,54 @@
 //
-//  ChecklistViewController.swift
+//  ViewController.swift
 //  EmojiClicker
 //
-//  Created by Erbo Shan on 2019-01-31.
+//  Created by Erbo Shan on 2019-01-28.
 //  Copyright © 2019 Erbo Shan. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+//class ViewController: UIViewController {
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        // Do any additional setup after loading the view, typically from a nib.
+//
+//    }
+//    @IBAction func showMessage(sender: UIButton){
+//        // Create dictionary of emoji icons
+//        var emojiDic : [String : String] = ["🇨🇳" : "China",
+//                                            "🇯🇵" :"Japan",
+//                                            "🇰🇷" : "Korea",
+//                                            "🇹🇼" : "Taiwan",
+//                                            "🇨🇦" : "Canada"]
+//        //var countryToLookUp = "🇹🇼"//
+//        //var country = emojiDic[countryToLookUp]//
+//        var country: String?
+//        let selectButton = sender
+//        if let countryToLookUp = selectButton.titleLabel?.text{
+//            country = emojiDic[countryToLookUp]
+//        }
+//
+//
+//        let alertController = UIAlertController(title: "Country", message: country, preferredStyle: UIAlertController.Style.alert)
+//        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+//        present(alertController, animated: true, completion: nil)
+//
+//    }
+//
+//}
+class ChecklistViewController: UITableViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
+        return cell
+    }
+
+}
+
